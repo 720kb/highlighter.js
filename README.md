02e49b3102ea1820601c5e0536c919ff85e1b977
45kb <filippo.oretti@gmail.com>
2015-07-01 11:35:42 +0200

    something more

--- README.md
+++ README.md
@@ -1,8 +1,32 @@
-```
-setInterval(function () {
+###Default
+if no next elements to select restart selecting the first element in the dom
+if no prev elements to select restart selecting the first element in the dom
 
-    window.Selez.skipNext(5);
-    window.Selez.selectNext();
-    window.Selez.highlight();
-  },800);
-```
+###Select next element
+window.Selez.selectNext()
+
+###Select prev element
+window.Selez.selectPrev()
+
+###Highlight element
+window.Selez.highlight();
+
+###De-highlight
+window.Selez.dehighlight();
+
+###Select Next by ID
+window.selectNext('#test');
+
+###Select Next by class/es
+window.selectNext('.class .class-2');
+
+###Select Prev by ID
+window.selectPrev('#test');
+
+###Select Prev by class/es
+window.selectPrev('.class .class-2');
+
+###TO-DO
+
+1 - events on selection and errors
+2 - select by tag name (next and prev)
--- index.js
+++ index.js
@@ -8,31 +8,69 @@
     Selez.dom = document.body.getElementsByTagName('*');
     Selez.selector =  Selez.dom[Selez.item];
 
-    Selez.highlight =  function () {
-      Selez.selector.style.transition = 'outline 0.15s linear';
+    Selez.highlight =  function highlightSelectedElement() {
+      Selez.selector.scrollIntoView();
+      window.scrollTo(0,Number(Selez.selector.offsetTop - 100).toFixed(0));
+      Selez.selector.style.transition = 'outline 0.55s linear';
       Selez.selector.style.outline = '3px inset red';
       Selez.selector.style.outlineOffset = '-2px';
     };
 
-    Selez.dehighlight =  function dehighlightSelector() {
+    Selez.dehighlight =  function dehighlightSelectedElement() {
       Selez.selector.style.transition = 'outline 0s linear';
       Selez.selector.style.outline = 'inherit';
       Selez.selector.style.outlineOffset = 'inherit';
     };
 
-    Selez.selectNext =  function selectNextElement() {
+    Selez.selectNext =  function selectNextElement(idOrClass) {
 
-      Selez.item += 1;
+      var i = Selez.item;
 
-      if (Selez.dom[Selez.item]) {
+      if (!idOrClass) {
 
-        Selez.dehighlight(Selez.selector);
-        Selez.selector = Selez.dom[Selez.item];
-        //new selected element event
+        Selez.item += 1;
+
+        if (Selez.dom[Selez.item]) {
+
+          Selez.selector = Selez.dom[Selez.item];
+          //new selected element event
+        } else {
+          //no next element restart from first element
+          Selez.item = 0;
+          Selez.selector =  Selez.dom[Selez.item];
+          window.console.info('No next elements restarting from the first element in page');
+        }
       } else {
-        //no next element to select event
-        Selez.item -= 1;
-        window.console.info('No next elements to select');
+        //if it's #id
+        if (idOrClass.indexOf('#') !== -1) {
+        for (i; i <= Selez.dom.length; i += 1) {
+
+          if (Selez.dom[i]
+            && Selez.dom[i].id
+            && Selez.dom[i].id.toString() === idOrClass.replace('#', '')) {
+
+            Selez.selector = Selez.dom[i];
+            Selez.item = i + 1;
+            break;
+          }
+        }
+        //if its .class/.class .es
+        } else if (idOrClass.indexOf('.') !== -1) {
+          for (i; i <= Selez.dom.length; i += 1) {
+
+            if (Selez.dom[i]
+              && Selez.dom[i].className
+              && Selez.dom[i].className.toString().indexOf(idOrClass.replace('.', '')) !== -1) {
+
+              Selez.selector = Selez.dom[i];
+              Selez.item = i + 1;
+              break;
+            }
+          }
+        } else {
+
+          window.console.error('You must set a correct #id or .class parameter');
+        }
       }
     };
 
@@ -42,13 +80,13 @@
 
       if (Selez.dom[Selez.item]) {
 
-        Selez.dehighlight(Selez.selector);
         Selez.selector = Selez.dom[Selez.item];
         //new selected element event
       } else {
         //no prev element to select event
-        Selez.item += 1;
-        window.console.info('No previous elements to select');
+        Selez.item = 0;
+        Selez.selector =  Selez.dom[Selez.item];
+        window.console.info('No previous elements restarting from the first element in page');
       }
     };
 
