ef680dceb688284a977692253634f6d8fd62d786
45kb <filippo.oretti@gmail.com>
2015-08-02 08:37:04 +0200

    Events closes #2

--- src/js/highlighter.js
+++ src/js/highlighter.js
@@ -24,6 +24,13 @@ window.smoothScroll=function(){if(void 0!==document.querySelectorAll&&void 0!==w
     var newDOM = []
       , i = 0;
 
+    this.onSelectedEvent = new window.CustomEvent('Highlighter:selected');
+    this.onUnderlinedEvent = new window.CustomEvent('Highlighter:underlined');
+    this.onErasedEvent = new window.CustomEvent('Highlighter:erased');
+    this.onSkippedEvent = new window.CustomEvent('Highlighter:skipped');
+    this.onScrolledEvent = new window.CustomEvent('Highlighter:scrolled');
+    this.onRestartEvent = new window.CustomEvent('Highlighter:restart');
+
     this.position = 0;
     this.dom = window.document.body.getElementsByTagName('*');
     this.element = this.dom[this.position];
@@ -59,6 +66,10 @@ window.smoothScroll=function(){if(void 0!==document.querySelectorAll&&void 0!==w
     if (this.scroll) {
 
       window.smoothScroll(element, this.scrollDuration);
+      this.onScrolledEvent.eventData = {
+        'element': element
+      };
+      window.dispatchEvent(this.onScrolledEvent);
     }
   };
   Highlighter.prototype.underline = function underlineSelectedElement() {
@@ -70,6 +81,10 @@ window.smoothScroll=function(){if(void 0!==document.querySelectorAll&&void 0!==w
       this.element.style.transition = 'outline 0.55s linear';
       this.element.style.outline = '3px inset #08FD31';
       this.element.style.outlineOffset = '-2px';
+      this.onUnderlinedEvent.eventData = {
+        'element': this.element
+      };
+      window.dispatchEvent(this.onUnderlinedEvent);
     }
   };
 
@@ -79,6 +94,10 @@ window.smoothScroll=function(){if(void 0!==document.querySelectorAll&&void 0!==w
       && this.element.style) {
       this.element.style.transition = 'outline none';
       this.element.style.outline = 'none';
+      this.onErasedEvent.eventData = {
+        'element': this.element
+      };
+      window.dispatchEvent(this.onErasedEvent);
     }
   };
 
@@ -87,6 +106,10 @@ window.smoothScroll=function(){if(void 0!==document.querySelectorAll&&void 0!==w
     this.position = 0;
     this.element = this.dom[this.position];
     this.next(identifier);
+    this.onSelectedEvent.eventData = {
+      'element': this.element
+    };
+    window.dispatchEvent(this.onSelectedEvent);
   };
 
   Highlighter.prototype.arrayCompared = function comparedArray(firstArray, secondArray) {
@@ -112,10 +135,15 @@ window.smoothScroll=function(){if(void 0!==document.querySelectorAll&&void 0!==w
 
             this.element = this.dom[i];
             this.position = i + 1;
+            this.onSelectedEvent.eventData = {
+              'element': this.element
+            };
+            window.dispatchEvent(this.onSelectedEvent);
             break;
           }
           if (i >= this.dom.length) {
             this.position = 0;
+            window.dispatchEvent(this.onRestartEvent);
             window.console.info('No next elements, restarting from the first element in page');
             break;
           }
@@ -137,11 +165,16 @@ window.smoothScroll=function(){if(void 0!==document.querySelectorAll&&void 0!==w
 
               this.element = this.dom[i];
               this.position = i + 1;
+              this.onSelectedEvent.eventData = {
+                'element': this.element
+              };
+              window.dispatchEvent(this.onSelectedEvent);
               break;
             }
           }
           if (i >= this.dom.length) {
             this.position = 0;
+            window.dispatchEvent(this.onRestartEvent);
             window.console.info('No next elements, restarting from the first element in page');
             break;
           }
@@ -156,10 +189,15 @@ window.smoothScroll=function(){if(void 0!==document.querySelectorAll&&void 0!==w
 
             this.element = this.dom[i];
             this.position = i + 1;
+            this.onSelectedEvent.eventData = {
+              'element': this.element
+            };
+            window.dispatchEvent(this.onSelectedEvent);
             break;
           }
           if (i >= this.dom.length) {
             this.position = 0;
+            window.dispatchEvent(this.onRestartEvent);
             window.console.info('No next elements, restarting from the first element in page');
             break;
           }
@@ -176,9 +214,16 @@ window.smoothScroll=function(){if(void 0!==document.querySelectorAll&&void 0!==w
 
         this.position = 0;
         this.element = this.dom[this.position];
+        window.dispatchEvent(this.onRestartEvent);
         window.console.info('No next elements, restarting from the first element in page');
       }
+
       this.element = this.dom[this.position];
+
+      this.onSelectedEvent.eventData = {
+        'element': this.element
+      };
+      window.dispatchEvent(this.onSelectedEvent);
     }
   };
 
@@ -197,10 +242,15 @@ window.smoothScroll=function(){if(void 0!==document.querySelectorAll&&void 0!==w
 
             this.element = this.dom[i];
             this.position = i - 1;
+            this.onSelectedEvent.eventData = {
+              'element': this.element
+            };
+            window.dispatchEvent(this.onSelectedEvent);
             break;
           }
           if (i < this.dom.length) {
             this.position = 0;
+            window.dispatchEvent(this.onRestartEvent);
             window.console.info('No previous elements, restarting from the first element in page');
             break;
           }
@@ -222,11 +272,16 @@ window.smoothScroll=function(){if(void 0!==document.querySelectorAll&&void 0!==w
 
               this.element = this.dom[i];
               this.position = i - 1;
+              this.onSelectedEvent.eventData = {
+                'element': this.element
+              };
+              window.dispatchEvent(this.onSelectedEvent);
               break;
             }
           }
           if (i < this.dom.length) {
             this.position = 0;
+            window.dispatchEvent(this.onRestartEvent);
             window.console.info('No previous elements, restarting from the first element in page');
             break;
           }
@@ -241,10 +296,15 @@ window.smoothScroll=function(){if(void 0!==document.querySelectorAll&&void 0!==w
 
             this.element = this.dom[i];
             this.position = i - 1;
+            this.onSelectedEvent.eventData = {
+              'element': this.element
+            };
+            window.dispatchEvent(this.onSelectedEvent);
             break;
           }
           if (i < this.dom.length) {
             this.position = 0;
+            window.dispatchEvent(this.onRestartEvent);
             window.console.info('No previous elements, restarting from the first element in page');
             break;
           }
@@ -260,9 +320,14 @@ window.smoothScroll=function(){if(void 0!==document.querySelectorAll&&void 0!==w
       if (this.position < 0) {
 
         this.position = 0;
+        window.dispatchEvent(this.onRestartEvent);
         window.console.info('No previous elements, restarting from the first element in page');
       }
       this.element = this.dom[this.position];
+      this.onSelectedEvent.eventData = {
+        'element': this.element
+      };
+      window.dispatchEvent(this.onSelectedEvent);
     }
   };
 
@@ -270,12 +335,17 @@ window.smoothScroll=function(){if(void 0!==document.querySelectorAll&&void 0!==w
     if (Number(skip) > 0) {
 
       this.position += skip;
+      this.onSkippedEvent.eventData = {
+        'element': this.dom[this.position]
+      };
+      window.dispatchEvent(this.onSkippedEvent);
     }
 
     if (Number(skip) > this.dom.length) {
 
       this.position = 0;
       this.element = this.dom[this.position];
+      window.dispatchEvent(this.onRestartEvent);
       window.console.log('No next elements, restarting from the first DOM element');
     }
   };
@@ -284,12 +354,17 @@ window.smoothScroll=function(){if(void 0!==document.querySelectorAll&&void 0!==w
     if (Number(skip) > 0) {
 
       this.position -= skip;
+      this.onSkippedEvent.eventData = {
+        'element': this.dom[this.position]
+      };
+      window.dispatchEvent(this.onSkippedEvent);
     }
 
     if (this.position < 0) {
 
       this.position = 0;
       this.element = this.dom[this.position];
+      window.dispatchEvent(this.onRestartEvent);
       window.console.log('No previous elements, restarting from the first DOM element');
     }
   };
